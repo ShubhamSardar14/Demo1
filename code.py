c8c65c3ac1894afa0b4a59eb6e543be01b9c1eb3
ShubhamSardar14 <87054843+ShubhamSardar14@users.noreply.github.com>
2022-03-16 09:14:28 +0530

    code python

---
+++ code.py
@@ -0,0 +1,9 @@
+class Stud{
+
+punlic static void main(String arg[]){
+System.out.prinln("Hello there");
+System.out.prinln("Name- Shubham Sardar");
+System.out.prinln("Batch - B");
+System.out.prinln("CDAC NOIDA");
+|
+}
